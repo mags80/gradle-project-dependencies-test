dd8eb625464fa99087b58f773c65085719f4de1d
Magnus Skuland <magnus@jpro.no>
2015-12-15 23:06:39 +0100

    Tested with Gradle version 2.8 as well.

--- directory/subproject2/src/main/java/no/gradle/example/subproject2/Foo.java
+++ directory/subproject2/src/main/java/no/gradle/example/subproject2/Foo.java
@@ -3,11 +3,13 @@ package no.gradle.example.subproject2;
 
 import net.webservicex.GetWeather;
 import no.gradle.example.library.MyLibraryImpl;
+import org.joda.time.DateTime;
 
 public class Foo {
 
     public void helloWorld() {
         new GetWeather().getCountryName();
+        new DateTime();
         System.out.println("Hello: " + new MyLibraryImpl().doSomeThingSmart());
     }
 
