2fed7e432c5a94db843d7ccd3d3f0c44a2f1c76b
Magnus Skuland <magnus@jpro.no>
2015-12-15 22:42:16 +0100

    Using one of the generated classes located under buildDir of library dependency

--- directory/subproject2/src/main/java/no/gradle/example/subproject2/Foo.java
+++ directory/subproject2/src/main/java/no/gradle/example/subproject2/Foo.java
@@ -1,11 +1,13 @@
 package no.gradle.example.subproject2;
 
 
+import net.webservicex.GetWeather;
 import no.gradle.example.library.MyLibraryImpl;
 
 public class Foo {
 
     public void helloWorld() {
+        new GetWeather().getCountryName();
         System.out.println("Hello: " + new MyLibraryImpl().doSomeThingSmart());
     }
 
